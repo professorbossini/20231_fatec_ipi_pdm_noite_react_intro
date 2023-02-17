0927be854368fba24b72245f0a8343febcab962b
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-17 20:25:14 -0300

    apostila03: fim da apostila

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles.css'
 // {nome: "Joao", idade: 18}
 const App = () => {
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+    const textoDoRotulo = 'Nome:'
+    const obterTextoDoBotao = () => 'Enviar'
+    const testeClique = () =>  alert("Clicou...")
     return (
         <div 
             style={{width: 768, margin: 'auto', backgroundColor: '#EEE', padding: 12, borderRadius: 8 }}>
-            <label 
+            <label
+                className='rotulo' 
                 htmlFor='nome' 
                 style={{display: 'block', marginBottom: 4}}>
-                    Nome:
+                    {textoDoRotulo}
             </label>
             <input 
                 type='text' 
                 id='nome' 
                 style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}}></input>
 
-            <button id="enviar" style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+            <button 
+                onClick={testeClique}
+                id="enviar"
+                style={estilosBotao}>{obterTextoDoBotao()}
+            </button>
 
 
         </div>
